13606e035c5fea1757e79820929d9da8fdca4eb5
Cédric Bodet <bodetc@gmail.com>
2017-03-14 23:56:16 +0100

    Added generator

---
+++ clone_generator.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from src.generator import read_csv, generator
+from src.models import nvidia
+
+samples = read_csv('./data', '\\')
+
+print("Number of lines in csv =", np.alen(samples))
+
+from sklearn.model_selection import train_test_split
+
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+n_train = np.alen(train_samples) * 6
+n_validation = np.alen(validation_samples) * 6
+print("Number of training examples =", n_train)
+print("Number of validation examples =", n_validation)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=64)
+validation_generator = generator(validation_samples, batch_size=64)
+
+model = nvidia()
+
+model.compile(loss='mse', optimizer='adam')
+history_object = \
+    model.fit_generator(train_generator, samples_per_epoch=n_train,
+                        validation_data=validation_generator, nb_val_samples=n_validation,
+                        nb_epoch=3)
+
+model.save('./model.h5')
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+import matplotlib.pyplot as plt
+
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
---
+++ src/generator.py
@@ -0,0 +1,60 @@
+import csv
+
+import cv2
+import numpy as np
+import sklearn
+from sklearn.utils import shuffle
+
+
+def read_csv(folder, separator):
+    samples = []
+    with open(folder + '/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            for i in range(0, 3):
+                path = line[i]
+                filename = path.split(separator)[-1]
+                line[i] = folder + '/IMG/' + filename
+            samples.append(line)
+    return samples
+
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:  # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset + batch_size]
+
+            images = []
+            measurements = []
+            for batch_sample in batch_samples:
+                measurement = float(batch_sample[3])
+                correction = 0.2
+
+                center = cv2.imread(batch_sample[0])
+                left = cv2.imread(batch_sample[1])
+                right = cv2.imread(batch_sample[2])
+
+                measurements.append(measurement)
+                images.append(center)
+
+                measurements.append(measurement + correction)
+                images.append(left)
+
+                measurements.append(measurement - correction)
+                images.append(right)
+
+            augmented_images = []
+            augmented_measurements = []
+            for image, measurement in zip(images, measurements):
+                augmented_images.append(image)
+                augmented_measurements.append(measurement)
+
+                augmented_images.append(cv2.flip(image, 1))
+                augmented_measurements.append(-measurement)
+
+            # trim image to only see section with road
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_measurements)
+            yield sklearn.utils.shuffle(X_train, y_train)
